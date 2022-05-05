0a754ac3849b87df3bd913ef52418e6fc7fe5029
ryan bautista <96922150+xryanb@users.noreply.github.com>
2022-05-05 11:52:33 -0700

    Add files via upload

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+    
+
+
+
+const PersonCard = props => {
+    return (
+        <div>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <p>age: {props.age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+        </div>
+
+
+
+);
+    }
+
+    
+export default PersonCard;
